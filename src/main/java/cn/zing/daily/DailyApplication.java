3498ca36de6bfd4dae0d28c64c1fedcaf1f04f43
dcy <1329851034@qq.com>
2023-07-17 19:57:31 +0800

    初始化提交

---
+++ src/main/java/cn/zing/daily/DailyApplication.java
@@ -0,0 +1,19 @@
+package cn.zing.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: dcy
+ * @create: 2023-07-17 19:48
+ */
+@SpringBootApplication
+public class DailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyApplication.class, args);
+    }
+}
+
+
