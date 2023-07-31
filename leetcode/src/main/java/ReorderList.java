dd5da6c2196a91892b7938f71a2d4105766f7d6a
dcy <1329851034@qq.com>
2023-07-31 11:30:26 +0800

    143. 重排链表

---
+++ leetcode/src/main/java/ReorderList.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 143. 重排链表 https://leetcode.cn/problems/reorder-list/
+ * <p>
+ * 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+ * L0 → L1 → … → Ln - 1 → Ln
+ * <p>
+ * 请将其重新排列后变为：
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * @author: dcy
+ * @create: 2023-07-31 11:12
+ */
+public class ReorderList {
+
+
+    /**
+     * 作者：力扣官方题解
+     * 链接：https://leetcode.cn/problems/reorder-list/solutions/452867/zhong-pai-lian-biao-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     */
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node);
+            node = node.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+}
+
+
