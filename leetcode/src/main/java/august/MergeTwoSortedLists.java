e77fbcac9ddd5103fac4aa92a358982e40d42a5b
dcy <1329851034@qq.com>
2023-08-05 12:57:08 +0800

    21. 合并两个有序链表

---
+++ leetcode/src/main/java/august/MergeTwoSortedLists.java
@@ -0,0 +1,68 @@
+package august;
+
+/**
+ * @description: 21. 合并两个有序链表 https://leetcode.cn/problems/merge-two-sorted-lists/
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * @author: dcy
+ * @create: 2023-08-05 12:37
+ */
+public class MergeTwoSortedLists {
+
+
+    /**
+     * 作者：力扣官方题解
+     * 链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/226408/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
