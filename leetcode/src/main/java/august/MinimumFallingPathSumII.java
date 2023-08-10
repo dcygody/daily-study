307e46b18f8ea7514ec9fa9633938faf2914c779
dcy <1329851034@qq.com>
2023-08-10 17:36:59 +0800

    1289. 下降路径最小和 II

---
+++ leetcode/src/main/java/august/MinimumFallingPathSumII.java
@@ -0,0 +1,69 @@
+package august;
+
+/**
+ * @description: 1289. 下降路径最小和 II https://leetcode.cn/problems/minimum-falling-path-sum-ii/
+ *
+ * 给你一个 n x n 整数矩阵 grid ，请你返回 非零偏移下降路径 数字和的最小值。
+ *
+ * 非零偏移下降路径 定义为：从 grid 数组中的每一行选择一个数字，且按顺序选出来的数字中，相邻数字不在原数组的同一列
+ *
+ *
+ * 输入：grid = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：13
+ * 解释：
+ * 所有非零偏移下降路径包括：
+ * [1,5,9], [1,5,7], [1,6,7], [1,6,8],
+ * [2,4,8], [2,4,9], [2,6,7], [2,6,8],
+ * [3,4,8], [3,4,9], [3,5,7], [3,5,9]
+ * 下降路径中数字和最小的是 [1,5,7] ，所以答案是 13 。
+ *
+ *
+ * 输入：grid = [[7]]
+ * 输出：7
+ *
+ *
+ * @author: dcy
+ * @create: 2023-08-10 17:27
+ */
+public class MinimumFallingPathSumII {
+
+
+    /**
+     *
+     *
+     作者：力扣官方题解
+     链接：https://leetcode.cn/problems/minimum-falling-path-sum-ii/solutions/101728/xia-jiang-lu-jing-zui-xiao-he-ii-by-leetcode-solut/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        int[][] d = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                d[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        System.arraycopy(grid[0], 0, d[0], 0, n);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (j == k) {
+                        continue;
+                    }
+                    d[i][j] = Math.min(d[i][j], d[i - 1][k] + grid[i][j]);
+                }
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            res = Math.min(res, d[n - 1][j]);
+        }
+        return res;
+    }
+
+
+
+}
+
+
